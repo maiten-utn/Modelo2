36f535a33038ec40a0f420414686b1a9df39c0d5
Emiliano C <emilianito528@gmail.com>
2024-11-20 21:41:48 -0300

    Modelo 2 rama F

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,8 +9,6 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
